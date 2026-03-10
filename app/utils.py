800b2aee1d52c5833b21193cd418429f2966689c
jahnavi besabathini <besabathinijahnavi@gmail.com>
2026-03-10 19:10:11 +0530

    Update model path to use BASE_DIR
    Refactor model path to be relative to the project structure.

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,14 @@
+import os
 from sentence_transformers import SentenceTransformer, util
 
-MODEL_PATH = r"C:\Users\DELL\OneDrive\Desktop\resume-job-description-fit\models\sentence_bert"
-model = SentenceTransformer(MODEL_PATH)
+# Get the base directory (one level above the current file)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Path to your model folder inside your repo
+MODEL_PATH = os.path.join(BASE_DIR, "models", "sentence_bert")
+
+# Load the model
+model = SentenceTransformer(MODEL_PATH)
 def predict_fit(resume_text, job_description):
 
     emb1 = model.encode([resume_text], convert_to_tensor=True)
